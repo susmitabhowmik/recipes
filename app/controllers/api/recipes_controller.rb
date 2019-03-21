a8671726db9cc75e67f18883b688290014613b24
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-20 22:21:38 -0400

    make API request to Edamam API, use figaro gem to securely store credentials

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,7 +1,7 @@
 class Api::RecipesController < ApplicationController
   def index
     search_term = params[:search]
-    response = HTTP.get()
-    p response.parse
+    response = HTTP.get ("https://api.edamam.com/search?q=#{search_term}&app_id=#{ENV["application_id"]}&app_key=#{ENV["api_key"]}")
+    render json: response.parse
   end
 end
